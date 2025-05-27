c9adc14fd3954e74b35955b9452265153d810595
LiviaSobral <liviaheloisa2005@gmail.com>
2025-05-27 14:03:26 -0300

    Feature: Model Usuario feita

---
+++ show/src/main/java/com/mycompany/show/model/User.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.show.model;
+
+/**
+ *
+ * @author LIVIAHELOISAALVESSOB
+ */
+public class User {
+    int pontos = 0;
+    int perguntas = 0;
+    boolean erro = false;
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }
+
+    public int getPerguntas() {
+        return perguntas;
+    }
+
+    public void setPerguntas(int perguntas) {
+        this.perguntas = perguntas;
+    }
+
+    public boolean isErro() {
+        return erro;
+    }
+
+    public void setErro(boolean erro) {
+        this.erro = erro;
+    }
+
+    
+    
+    
+}
